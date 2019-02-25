5612d43e25b86085bacbd32136ebf3ddcf76fe92
AndyoyoZ <827020523@qq.com>
2019-02-25 11:02:39 +0800

    使转换的点云能够显示出来
    TODO:ROS消息同步

--- depth2cloudV0.2/src/depth2cloud_node.cpp
+++ depth2cloudV0.2/src/depth2cloud_node.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <ros/ros.h>
 #include <pcl/point_cloud.h>
@@ -11,97 +10,86 @@
 #include <cv_bridge/cv_bridge.h>
 #include "depth2cloud.hpp"
 
-// boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-cv::Mat rgb,ir,depth;
-bool saveimages_flag = false;
-int imagecount=0;
-std::vector<int> params;
-
-void readImage(const sensor_msgs::Image::ConstPtr msgImage, cv::Mat &image) 
-{
-    cv_bridge::CvImageConstPtr pCvImage;
-    pCvImage = cv_bridge::toCvShare(msgImage, msgImage->encoding);
-    pCvImage->image.copyTo(image);
-}
-void irimageCB(const sensor_msgs::Image::ConstPtr msgImage)
-{
-    readImage(msgImage,ir);
-    cv::imshow("ir image", ir);
-    cv::waitKey(10);
-}
-
-void rgbimageCB(const sensor_msgs::Image::ConstPtr msgImage)
-{
-    readImage(msgImage,rgb);
-    cv::imshow("rgb image", rgb);
-    cv::waitKey(10);
-}
-
-void depthimageCB(const sensor_msgs::Image::ConstPtr msgImage)
-{
-    readImage(msgImage,depth);
-    cv::imshow("depth image", depth);
-    cv::waitKey(10);
-}
-
-// void pointcloudShow(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
-// {
-//     //显示点云
-//     // pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointXYZRGB);  
-//     viewer->setBackgroundColor (0, 0, 0);
-//     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb_(cloud);
-//     viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb_, "color cloud");
-//     viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "color cloud");
-//     viewer->addCoordinateSystem (1.0);
-//     viewer->initCameraParameters ();
-// }
-
-void saveimages()
-{
-    if(saveimages_flag)
-    {
-        saveimages_flag = false;
-        cv::imwrite("myrgb.png",rgb,params);
-        cv::imwrite("myir.png",ir,params);
-        cv::imwrite("mydepth.png",depth,params);
-        ROS_INFO("Images saved...");
-    }
-}
-
 int main (int argc, char** argv)
 {
+    char key;
     ros::init(argc, argv, "pcl_write");
     ros::NodeHandle nh;
+
+    //图片显示窗口
     cv::namedWindow("rgb image");
     cv::namedWindow("ir image");
     cv::namedWindow("depth image");
     cv::startWindowThread();//开线程刷新图片显示窗口
 
-    params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    params.push_back(9);
+    //点云显示窗口
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+    // pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb_(color_cloud);
+    // viewer->addPointCloud<pcl::PointXYZRGB> (color_cloud, rgb_, "color cloud");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "color cloud");
+    viewer->addCoordinateSystem (1.0);
+    viewer->initCameraParameters ();
+
+    //从yaml文件中读取相机参数（IR相机内参，RGB相机与IR相机的外参以及单应矩阵）
+    trans_flag = loadyamlfiles();
 
     image_transport::ImageTransport it(nh);  
     image_transport::Subscriber rgbimg_sub = it.subscribe("/kinect2/cap/cap_bgr_rect", 1, rgbimageCB);
     image_transport::Subscriber irimg_sub = it.subscribe("/kinect2/sd/image_ir_rect", 1, irimageCB);
     image_transport::Subscriber depthimg_sub = it.subscribe("/kinect2/sd/image_depth_rect", 1, depthimageCB);
 
-    ros::Rate rate(1.0);
+    ros::Rate rate(10.0);
 
-    while (ros::ok() /*&& ! viewer->wasStopped()*/)
+    while (ros::ok() && ! viewer->wasStopped())
     {
-        if(cv::waitKey(10)=='s')
+        key = cv::waitKey(30);
+        switch(key)
         {
+            case 't':
+                trans_flag = true;
+                break;
+            case 's':
+                saveimages_flag = true;
+                savecloud_flag = true;
+                break;
+            default:  
+                break;  
+        }
+
+        if(trans_flag==true)
+        {
+            trans_flag = false;
             saveimages_flag = true;
-            saveimages();
+            savecloud_flag = true;
+            if(!rgb.empty() && !depth.empty() && !ir.empty())
+            {
+                ROS_INFO("Run RGB image to IR image...");
+                runRGB2IR(rgb,ir,homographyMatrix,trans);
+                ROS_INFO("Run depth map to color point cloud...");
+                runDepth2cloud(depth, trans, RT, color_cloud);
+                // viewer->removePointCloud("color cloud");
+                // viewer->addPointCloud<pcl::PointXYZRGB> (color_cloud, rgb_, "color cloud");
+                pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb_(color_cloud);
+                viewer->addPointCloud<pcl::PointXYZRGB> (color_cloud, rgb_, "color cloud");
+                ROS_INFO("Depth map to point cloud complete!");
+                ss << imagecount++;
+                saveimages();
+            }
+            else
+            {
+                ROS_INFO("Image is empty...");
+            }   
         }
-        else if(cv::waitKey(10)==27)
-        {
-            ROS_INFO("EXIT...");
-            return 0;
-        }
+
+        viewer->spinOnce(100);
+		boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+
         ros::spinOnce();
         rate.sleep();
     }
 
     return 0;
 }
+
+//TODO:点云拼接
